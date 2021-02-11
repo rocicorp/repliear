27d8155c217315f804448471cdb6dfdedce1e1e2
Aaron Boodman <aaron@aaronboodman.com>
2021-02-11 04:05:41 -1000

    env

--- examples/components/Mondrian.js
+++ examples/components/Mondrian.js
@@ -3,6 +3,7 @@ import Designer from '../../src/Designer';
 import Replicache from 'replicache';
 import {useSubscribe} from 'replicache-react-util';
 
+console.log('vercel', process.env);
 const rep = new Replicache({
   clientViewURL: new URL('/api/replicache-client-view', location.href).toString(),
   diffServerURL: process.env.VERCEL ? 'https://serve.replicache.dev/pull' : 'http://localhost:7001/pull',
