aeba6e6dafbeeb1c936c772fca0a9931c325301c
Aaron Boodman <aaron@aaronboodman.com>
2021-02-11 03:34:43 -1000

    Remove other old stuff

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import dynamic from 'next/dynamic'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 const Mondrian = dynamic(
   () => import('../examples/components/Mondrian'),
