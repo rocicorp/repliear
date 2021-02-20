3d90acd6603f4b70e744857806b24b837bce5d00
Aaron Boodman <aaron@aaronboodman.com>
2021-02-19 15:10:21 -1000

    Add cosmosdb push test

---
+++ pages/api/config.ts
@@ -0,0 +1,7 @@
+export default {
+  endpoint: "https://replidraw.documents.azure.com:443/",
+  key: "Wl0K3w4jCedslIsKlwsgXGOOaiiPz2uwM9HrmaSkkrrjnn3eFrSiy9zAZWXU3btml5gbis3B2pcRHhvJg0y0tg==",
+  databaseId: "replidraw",
+  containerId: "c1",
+  partitionKey: { kind: "Hash", paths: ["/partition"] }
+};
