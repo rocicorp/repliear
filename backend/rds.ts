162630d97e5d6c797d4f028310d0d33cfc593d31
Aaron Boodman <aaron@aaronboodman.com>
2021-03-02 13:51:24 -1000

    Response to code review: https://github.com/rocicorp/replidraw/pull/5
    Also added a LastModified column to Object table. We will use this
    eventually to do things like prune old client state.

--- backend/rds.ts
+++ backend/rds.ts
@@ -102,7 +102,10 @@ async function createDatabase() {
   await executeStatement(`CREATE TABLE Object (
     K VARCHAR(255) PRIMARY KEY NOT NULL,
     V TEXT NOT NULL,
-    Version BIGINT NOT NULL)`);
+    Version BIGINT NOT NULL,
+    LastModified TIMESTAMP NOT NULL
+      DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    INDEX (Version))`);
 
   await executeStatement(`INSERT INTO Cookie (Version) VALUES (0)`);
 
