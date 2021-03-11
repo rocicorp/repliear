35fb9d3341e9245f4a3ac3c18e80179f9ed438c8
Aaron Boodman <aaron@aaronboodman.com>
2021-03-11 11:53:46 -1000

    Put a TODO in the not-yet-working nav items

--- frontend/nav.tsx
+++ frontend/nav.tsx
@@ -28,24 +28,12 @@ export function Nav({ data }: { data: Data | null }) {
     });
   };
 
+  const soon = () => {
+    alert("Coming soon… 😔")
+  };
+
   return (
     <div className={styles.nav} style={{}}>
-      <div className={styles.button} title="Move">
-        <svg
-          width="18"
-          height="18"
-          viewBox="0 0 18 18"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M14.872 8.859L3.646 2.072l-.98-.592.231 1.121 2.683 13 .243 1.178.664-1.003 3.038-4.59 5.22-1.417 1.127-.306-1-.604zM4.108 3.52l9.247 5.59-4.274 1.16-.182.05-.104.156-2.479 3.746L4.108 3.52z"
-            fillRule="nonzero"
-            fillOpacity="1"
-            fill="white"
-            stroke="none"
-          ></path>
-        </svg>
-      </div>
       <div
         onClick={() => onRectangle()}
         className={styles.button}
@@ -64,7 +52,7 @@ export function Nav({ data }: { data: Data | null }) {
           ></path>
         </svg>
       </div>
-      <div className={styles.button} title="Ellipse">
+      <div onClick={() => soon()} className={styles.button} title="Ellipse">
         <svg
           width="18"
           height="18"
@@ -79,7 +67,7 @@ export function Nav({ data }: { data: Data | null }) {
           ></path>
         </svg>
       </div>
-      <div className={styles.button} title="Text">
+      <div onClick={() => soon()} className={styles.button} title="Text">
         <svg
           width="18"
           height="18"
