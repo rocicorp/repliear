fa3a85ff39b832804692d646976939e2bcceaefb
Cesar Alaestante <cesara@gmail.com>
2022-04-08 22:08:34 -0700

    feat: working comment pull, simplified structure

--- tools/get-github-issues.ts
+++ tools/get-github-issues.ts
@@ -26,100 +26,87 @@ async function mainImpl(
   console.log("gitHubKey:\t", gitHubKey);
   console.log("gitHubRepo:\t", gitHubRepo);
 
-  const totalClosedIssueCount = await getIssueCount(gitHubKey, "closed");
-  const totalOpenIssueCount = await getIssueCount(gitHubKey, "open");
+  const totalIssueCount = await getIssueCount(gitHubKey);
 
-  console.log("totalClosedIssueCount: ", totalClosedIssueCount);
-  console.log("totalOpenIssueCount: ", totalOpenIssueCount);
+  console.log("totalIssueCount: ", totalIssueCount);
 
   const MAX_ISSUE_PER_PAGE = 100;
-  const totalOpenIssuePage = Math.ceil(
-    (totalOpenIssueCount - 1) / MAX_ISSUE_PER_PAGE + 1
+  const totalAllIssuePage = Math.floor(
+    (totalIssueCount - 1) / MAX_ISSUE_PER_PAGE + 1
   );
-  const totalClosedIssuePage = Math.ceil(
-    (totalClosedIssueCount - 1) / MAX_ISSUE_PER_PAGE + 1
-  );
-  const issuesOpen = await getIssues(gitHubKey, "open", totalOpenIssuePage);
-  const issuesClosed = await getIssues(
-    gitHubKey,
-    "closed",
-    totalClosedIssuePage
-  );
-  const allIssues = [...issuesOpen, ...issuesClosed];
+
+  const allIssues = await getIssues(gitHubKey, totalAllIssuePage);
+
+  // const allIssues = [...issuesOpen, ...issuesClosed];
   stdout.write(JSON.stringify(allIssues, null, 4));
   process.exit(0);
 }
 
-async function getIssueCount(
-  gitHubKey: string,
-  state: "open" | "closed"
-): Promise<number> {
-  const issueCountUrl = (state: "open" | "closed") =>
-    `https://api.github.com/search/issues?q=repo:${gitHubRepo}+type:issue+state:${state}&page=0&per_page=1`;
+async function getIssueCount(gitHubKey: string): Promise<number> {
+  const issueCountUrl = `https://api.github.com/search/issues?q=repo:${gitHubRepo}+is:issue&page=0&per_page=1`;
 
-  return await fetch(issueCountUrl(state), {
+  const response = await fetch(issueCountUrl, {
     headers: {
       "Content-Type": "application/json",
       Authorization: `token ${gitHubKey}`,
     },
-  })
-    // @ts-ignore
-    .then((response) => response.json())
-    // @ts-ignore
-    .then((data) => {
-      return data.total_count;
-    });
+  });
+
+  const issues = await response.json();
+  return issues.total_count;
 }
 
 async function getIssues(
   gitHubKey: string,
-  state: "open" | "closed",
   totalPages: number
 ): Promise<any[]> {
   const MAX_ISSUE_PER_PAGE = 100;
+  const issueUrl = (page: number) =>
+    `https://api.github.com/search/issues?q=repo:${gitHubRepo}+is:issue&page=${page}&per_page=${MAX_ISSUE_PER_PAGE}`;
 
-  const issueUrl = (state: "open" | "closed", page: number) =>
-    `https://api.github.com/repos/${gitHubRepo}/issues?state=${state}&per_page=${MAX_ISSUE_PER_PAGE}&page=${page}`;
   let issues = [];
   for (let i = 1; i <= totalPages; i++) {
-    const dots = ".".repeat(i);
-    const left = totalPages - i;
-    const empty = " ".repeat(left);
-    process.stdout.write(`\r[${dots}${empty}] ${i}/${totalPages}`);
-    let pagedIssues = await fetch(issueUrl(state, i), {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `token ${gitHubKey}`,
-      },
-    })
-      // @ts-ignore
-      .then((response) => response.json())
-      // @ts-ignore
-      .then((data) => {
-        return data;
-      });
+    process.stdout.write(
+      `\r[${Math.round((i / totalPages) * 100)}%] ${i}/${totalPages}`
+    );
 
-    pagedIssues = await fillComments(pagedIssues, gitHubKey);
-    issues.push(pagedIssues);
-  }
-  return issues;
-}
-
-async function fillComments(pagedIssues: any[], gitHubKey: string) {
-  const commentedIssues = await pagedIssues.map(async (i) => {
-    let r = await fetch(i.comments_url, {
+    const url = issueUrl(i);
+    const response = await fetch(url, {
       headers: {
         "Content-Type": "application/json",
         Authorization: `token ${gitHubKey}`,
       },
     });
-    const commentJson = await r.json();
-    return {
-      ...i,
-      comments_expanded: commentJson
-    };
-  });
-  return await commentedIssues;
+
+    const responseIssues = await response.json();
+    let pagedIssues = responseIssues.items;
+    pagedIssues = await fillComments(pagedIssues, gitHubKey);
+    issues.push(pagedIssues);
+  }
+
+  return issues;
+}
+
+async function fillComments(pagedIssues: any[], gitHubKey: string) {
+  if (!pagedIssues) {
+    return pagedIssues;
+  }
+  const commentedIssues = Promise.all(
+    pagedIssues.map(async (i) => {
+      let r = await fetch(i.comments_url, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `token ${gitHubKey}`,
+        },
+      });
+      const commentJson = await r.json();
+      return {
+        ...i,
+        comments_expanded: commentJson,
+      };
+    })
+  );
+  return commentedIssues;
 }
 
 main(gitHubKey, gitHubRepo);
