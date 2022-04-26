f92f21048c2d8e72ed130f506db978e6b998589f
Gregory Baker <greg@roci.dev>
2022-04-26 13:34:04 -0700

    partial sync

--- backend/data.ts
+++ backend/data.ts
@@ -66,7 +66,7 @@ export async function createSchemaVersion1(executor: Executor) {
   await executor(`create index on entry (version)`);
 }
 
-export const SAMPLE_SPACE_ID = "sampleSpaceID-7";
+export const SAMPLE_SPACE_ID = "sampleSpaceID-8";
 
 export async function getUnusedSpace(
   executor: Executor,
