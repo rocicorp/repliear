7e9e232adedd1a833203beae70788b96f6ba3448
Cesar Alaestante <cesara@gmail.com>
2022-04-27 22:09:27 -0700

    dedupe issue-item

--- frontend/issue-col.tsx
+++ frontend/issue-col.tsx
@@ -1,17 +1,11 @@
 import StatusIcon from "./status-icon";
 import React, { CSSProperties, forwardRef, memo } from "react";
-import {
-  DraggableProvided,
-  DraggableStateSnapshot,
-  Droppable,
-  DroppableProvided,
-} from "react-beautiful-dnd";
+import { Droppable, DroppableProvided } from "react-beautiful-dnd";
 import type { Issue, Status } from "./issue";
 import IssueItem from "./issue-item";
 import { FixedSizeList } from "react-window";
 import AutoSizer from "react-virtualized-auto-sizer";
-import PriorityMenu from "./priority-menu";
-import classNames from "classnames";
+import IssueItemBase from "./issue-item-base";
 
 interface Props {
   status: Status;
@@ -24,54 +18,6 @@ interface RowProps {
   data: Array<Issue>;
   style: CSSProperties;
 }
-
-const IssueCloneItem = ({
-  issue,
-  provided,
-  snapshot,
-}: {
-  issue: Issue;
-  provided: DraggableProvided;
-  snapshot: DraggableStateSnapshot;
-}) => {
-  const isDragging = snapshot.isDragging && !snapshot.isDropAnimating;
-  return (
-    <div
-      style={{
-        ...provided.draggableProps.style,
-      }}
-      ref={provided.innerRef}
-      className={classNames(
-        "cursor-default flex flex-col w-11/12 px-4 py-3 mb-2 text-white rounded focus:outline-none",
-        {
-          /* eslint-disable @typescript-eslint/naming-convention */
-          "shadow-modal": isDragging,
-          "bg-gray-300": isDragging,
-          "bg-gray-400": !isDragging,
-          /* eslint-enable @typescript-eslint/naming-convention */
-        }
-      )}
-      {...provided.draggableProps}
-      {...provided.dragHandleProps}
-    >
-      <div className="flex justify-between w-full cursor-default">
-        <div className="flex flex-col">
-          <span className="text-xs font-normal uppercase">{issue.id}</span>
-          <span className="mt-1 text-sm font-medium text-gray-2 line-clamp-2 overflow-ellipsis h-10">
-            {issue.title}
-          </span>
-        </div>
-      </div>
-      <div className="mt-2.5 flex items-center">
-        <PriorityMenu
-          labelVisible={false}
-          priority={issue.priority}
-          onSelect={() => null}
-        />
-      </div>
-    </div>
-  );
-};
 
 const Row = ({ data: items, index, style }: RowProps) => (
   <div
@@ -110,7 +56,7 @@ function IssueCol({ title, status, issues }: Props) {
       renderClone={(provided, snapshot, rubric) => {
         const issue = issues[rubric.source.index];
         return (
-          <IssueCloneItem
+          <IssueItemBase
             issue={issue}
             provided={provided}
             snapshot={snapshot}
--- frontend/issue-item.tsx
+++ frontend/issue-item.tsx
@@ -1,5 +1,3 @@
-import classNames from "classnames";
-import PriorityMenu from "./priority-menu";
 import React, { memo } from "react";
 import {
   Draggable,
@@ -8,6 +6,7 @@ import {
 } from "react-beautiful-dnd";
 import type { Issue, Priority } from "./issue";
 import { queryTypes, useQueryStates } from "next-usequerystate";
+import IssueItemBase from "./issue-item-base";
 
 interface IssueProps {
   issue: Issue;
@@ -41,48 +40,14 @@ const IssueItem = ({ issue, index, onChangePriority }: IssueProps) => {
   return (
     <Draggable draggableId={issue.id || "id"} index={index} key={issue.id}>
       {(provided: DraggableProvided, snapshot: DraggableStateSnapshot) => {
-        const isDragging = snapshot.isDragging && !snapshot.isDropAnimating;
-        console.log("isDragging", isDragging);
         return (
-          <div
-            style={{
-              ...provided.draggableProps.style,
-            }}
-            ref={provided.innerRef}
-            className={classNames(
-              "cursor-default flex flex-col w-11/12 px-4 py-2 mb-2 text-white rounded focus:outline-none",
-              {
-                /* eslint-disable @typescript-eslint/naming-convention */
-                "shadow-modal": isDragging,
-                "bg-gray-300": isDragging,
-                "bg-gray-400": !isDragging,
-                /* eslint-enable @typescript-eslint/naming-convention */
-              }
-            )}
-            {...provided.draggableProps}
-            {...provided.dragHandleProps}
-          >
-            <div
-              className="flex justify-between w-full cursor-pointer"
-              onClick={handleIssueItemClick}
-            >
-              <div className="flex flex-col">
-                <span className="text-xs font-normal uppercase">
-                  {issue.id}
-                </span>
-                <span className="mt-1 text-sm font-medium text-gray-2 line-clamp-2 overflow-ellipsis h-10">
-                  {issue.title}
-                </span>
-              </div>
-            </div>
-            <div className="mt-2.5 flex items-center">
-              <PriorityMenu
-                onSelect={handleChangePriority}
-                labelVisible={false}
-                priority={issue.priority}
-              />
-            </div>
-          </div>
+          <IssueItemBase
+            issue={issue}
+            provided={provided}
+            snapshot={snapshot}
+            handleIssueItemClick={handleIssueItemClick}
+            handleChangePriority={handleChangePriority}
+          />
         );
       }}
     </Draggable>
