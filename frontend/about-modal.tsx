d671efe19dd1f6c6b8aa5bf61695b8e78a10fcb4
Aaron Boodman <aaron@aaronboodman.com>
2022-05-06 11:25:36 -1000

    About dialog copyediting.

--- frontend/about-modal.tsx
+++ frontend/about-modal.tsx
@@ -16,10 +16,6 @@ function H1({ children }: { children: string }) {
   return <div className="mt-5 text-lg font-normal text-gray-4">{children}</div>;
 }
 
-function H2({ children }: { children: string }) {
-  return <div className="mt-4 text-md font-normal text-gray-4">{children}</div>;
-}
-
 function P({
   noTopMargin,
   children,
@@ -29,7 +25,7 @@ function P({
 }) {
   return (
     <div
-      className={classNames("text-sm font-normal text-gray-5", {
+      className={classNames("text-sm font-normal text-gray-1", {
         // eslint-disable-next-line @typescript-eslint/naming-convention
         "mt-3": !noTopMargin,
       })}
@@ -46,21 +42,6 @@ function A({ href, children }: { href: string; children: string }) {
         {children}
       </a>
     </span>
-  );
-}
-
-function FeatureSection({
-  title,
-  children,
-}: {
-  title: string;
-  children?: React.ReactNode;
-}) {
-  return (
-    <>
-      <H2>{title}</H2>
-      <P>{children}</P>
-    </>
   );
 }
 
@@ -83,55 +64,59 @@ export default function AboutModal({ isOpen, onDismiss }: Props) {
       <div className="flex flex-col flex-1 px-8 pt-4 pb-8 overflow-y-auto">
         <P noTopMargin>
           Repliear is a loving &hearts; tribute to the{" "}
-          <A href="https://linear.app/">Linear issue tracker</A>, built with{" "}
-          <A href="https://replicache.dev">Replicache</A>,{" "}
-          <A href="https://nextjs.org">Next.js</A>, and{" "}
-          <A href="https://supabase.com">Supabase.</A>
-        </P>
-
-        <P>
-          Linear&apos;s killer feature is performance: Every interaction
-          throughout the UI responds instantly. There are zero spinners.
-          Additionally, the Linear UI continuously updates automatically from
-          the server. When a user changes something, other users&apos; views
-          update immediately to reflect the change.
+          <A href="https://linear.app/">Linear issue tracker</A> built with{" "}
+          <A href="https://replicache.dev">Replicache.</A>
         </P>
 
         <P>
           Replicache makes it much easier to build apps like Linear. To show
           how, this demo recreates a subset of the Linear experience, focusing
-          on the distinctive features of Linear that are especially difficult to
-          engineer.
+          on the distinctive features that are especially difficult to engineer.
         </P>
 
         <H1>Key Features</H1>
-        <FeatureSection title="Substantial Dataset">
-          This demo contains the entire React issue database as of mid-April
-          2022. It comprises about 11 thousands issues and about 50 MB of
-          structured data.
-        </FeatureSection>
-        <FeatureSection title="Instant UI Responsiveness">
-          Scrolling, issue creation, issue modification, and view transition are
-          all instant. There are no spinners, and every mutation is optimistic.
-          Even details like the number of active issues update immediately to
-          reflect changes.
-        </FeatureSection>
-        <FeatureSection title="Complex Filters">
-          Use the filter picker to build complex multi-attribute filters. The
-          response is instant, with zero network requests.
-        </FeatureSection>
-        <FeatureSection title="Realtime Sync Throughout">
-          Changes made by one user are instantly reflected to other users, even
-          if they are looking at a different view. Replicache syncs the entire
-          data model under the covers, and since the views are rendered from
-          that data model they update automatically when it changes.
-        </FeatureSection>
+        <ul
+          /* i was tired and needed to ship */
+          style={{
+            listStyleType: "circle",
+            padding: "1em",
+          }}
+          className="text-sm font-normal text-gray-1"
+        >
+          <li
+            style={{
+              marginBottom: "0.5em",
+            }}
+          >
+            <b>Dataset:</b> The entire React issue db as of April 2022. ~11k
+            issues, ~50 MB of data.
+          </li>
+          <li
+            style={{
+              marginBottom: "0.5em",
+            }}
+          >
+            <b>Spinner-free:</b> Everything in the UI responds instantly,
+            without progress bars.
+          </li>
+          <li
+            style={{
+              marginBottom: "0.5em",
+            }}
+          >
+            <b>Realtime sync:</b> Any change made by one user is seen ~instantly
+            by others. Open in two windows to test. Even works across views!
+          </li>
+          <li>
+            <b>Complex filters:</b> Use the filter picker in the top nav to
+            build complex filters.
+          </li>
+        </ul>
 
         <H1>Learn More</H1>
         <P>
-          Replicache is the best way to build applications like Linear. Learn
-          more at <A href="https://replicache.dev">replicache.dev</A>, or come
-          visit us in <A href="https://discord.replicache.dev">Discord</A>.
+          Learn more at <A href="https://replicache.dev">replicache.dev</A>, or
+          come visit us in <A href="https://discord.replicache.dev">Discord</A>.
         </P>
 
         <P>
