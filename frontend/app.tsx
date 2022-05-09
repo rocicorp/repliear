6ad8dc4cabd9decd0adbcf63bdcd4a067b393e88
Greg Baker <greg@roci.dev>
2022-05-09 16:24:36 -0700

    perf: don't refilter or resort if filter/orderby hasn't changed (#128)

--- frontend/app.tsx
+++ frontend/app.tsx
@@ -25,85 +25,102 @@ import TopFilter from "./top-filter";
 import IssueList from "./issue-list";
 import { useQueryState } from "next-usequerystate";
 import IssueBoard from "./issue-board";
-import { minBy, sortBy, sortedIndexBy } from "lodash";
+import { isEqual, minBy, sortBy, sortedIndexBy } from "lodash";
 import IssueDetail from "./issue-detail";
 import { generateKeyBetween } from "fractional-indexing";
 
-type Filters = {
-  readonly viewFilter: (issue: Issue) => boolean;
-  readonly issuesFilter: (issue: Issue) => boolean;
+class Filters {
+  private readonly _viewStatuses: Set<Status> | undefined;
+  private readonly _issuesStatuses: Set<Status> | undefined;
+  private readonly _issuesPriorities: Set<Priority> | undefined;
   readonly hasNonViewFilters: boolean;
-};
+  constructor(
+    view: string | null,
+    priorityFilter: string | null,
+    statusFilter: string | null
+  ) {
+    this._viewStatuses = undefined;
+    switch (view?.toLowerCase()) {
+      case "active":
+        this._viewStatuses = new Set([Status.IN_PROGRESS, Status.TODO]);
+        break;
+      case "backlog":
+        this._viewStatuses = new Set([Status.BACKLOG]);
+        break;
+      default:
+        this._viewStatuses = undefined;
+    }
+
+    this._issuesStatuses = undefined;
+    this._issuesPriorities = undefined;
+    this.hasNonViewFilters = false;
+    if (statusFilter) {
+      this._issuesStatuses = new Set<Status>();
+      for (const s of statusFilter.split(",")) {
+        const parseResult = statusEnumSchema.safeParse(s);
+        if (
+          parseResult.success &&
+          (!this._viewStatuses || this._viewStatuses.has(parseResult.data))
+        ) {
+          this.hasNonViewFilters = true;
+          this._issuesStatuses.add(parseResult.data);
+        }
+      }
+    }
+    if (!this.hasNonViewFilters) {
+      this._issuesStatuses = this._viewStatuses;
+    }
+
+    if (priorityFilter) {
+      this._issuesPriorities = new Set<Priority>();
+      for (const p of priorityFilter.split(",")) {
+        const parseResult = priorityEnumSchema.safeParse(p);
+        if (parseResult.success) {
+          this.hasNonViewFilters = true;
+          this._issuesPriorities.add(parseResult.data);
+        }
+      }
+      if (this._issuesPriorities.size === 0) {
+        this._issuesPriorities = undefined;
+      }
+    }
+  }
+
+  viewFilter(issue: Issue): boolean {
+    return this._viewStatuses ? this._viewStatuses.has(issue.status) : true;
+  }
+
+  issuesFilter(issue: Issue): boolean {
+    if (this._issuesStatuses) {
+      if (!this._issuesStatuses.has(issue.status)) {
+        return false;
+      }
+    }
+    if (this._issuesPriorities) {
+      if (!this._issuesPriorities.has(issue.priority)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  equals(other: Filters): boolean {
+    return (
+      this === other ||
+      (isEqual(this._viewStatuses, other._viewStatuses) &&
+        isEqual(this._issuesStatuses, other._issuesStatuses) &&
+        isEqual(this._issuesPriorities, other._issuesPriorities) &&
+        isEqual(this.hasNonViewFilters, other.hasNonViewFilters))
+    );
+  }
+}
 
 function getFilters(
   view: string | null,
   priorityFilter: string | null,
   statusFilter: string | null
 ): Filters {
-  let viewStatuses: Set<Status> | undefined = undefined;
-  switch (view?.toLowerCase()) {
-    case "active":
-      viewStatuses = new Set([Status.IN_PROGRESS, Status.TODO]);
-      break;
-    case "backlog":
-      viewStatuses = new Set([Status.BACKLOG]);
-      break;
-    default:
-      viewStatuses = undefined;
-  }
-
-  let issuesStatuses: Set<Status> | undefined = undefined;
-  let issuesPriorities: Set<Priority> | undefined = undefined;
-  let hasNonViewFilters = false;
-  if (statusFilter) {
-    issuesStatuses = new Set<Status>();
-    for (const s of statusFilter.split(",")) {
-      const parseResult = statusEnumSchema.safeParse(s);
-      if (
-        parseResult.success &&
-        (!viewStatuses || viewStatuses.has(parseResult.data))
-      ) {
-        hasNonViewFilters = true;
-        issuesStatuses.add(parseResult.data);
-      }
-    }
-  }
-  if (!hasNonViewFilters) {
-    issuesStatuses = viewStatuses;
-  }
-
-  if (priorityFilter) {
-    issuesPriorities = new Set<Priority>();
-    for (const p of priorityFilter.split(",")) {
-      const parseResult = priorityEnumSchema.safeParse(p);
-      if (parseResult.success) {
-        hasNonViewFilters = true;
-        issuesPriorities.add(parseResult.data);
-      }
-    }
-    if (issuesPriorities.size === 0) {
-      issuesPriorities = undefined;
-    }
-  }
-  return {
-    viewFilter: (issue: Issue) => {
-      return viewStatuses ? viewStatuses.has(issue.status) : true;
-    },
-    issuesFilter: (issue: Issue) => {
-      if (issuesStatuses) {
-        if (!issuesStatuses.has(issue.status)) {
-          return false;
-        }
-      }
-      if (issuesPriorities) {
-        if (!issuesPriorities.has(issue.priority)) {
-          return false;
-        }
-      }
-      return true;
-    },
-    hasNonViewFilters,
-  };
+  return new Filters(view, priorityFilter, statusFilter);
 }
 
 function getIssueOrder(view: string | null, orderBy: string | null): Order {
@@ -204,7 +221,10 @@ function reducer(
     return orderValue;
   }
   function filterAndSort(issues: Issue[]): Issue[] {
-    return sortBy(issues.filter(filters.issuesFilter), order);
+    return sortBy(
+      issues.filter((issue) => filters.issuesFilter(issue)),
+      order
+    );
   }
   function countViewIssues(issues: Issue[]): number {
     let count = 0;
@@ -287,6 +307,9 @@ function reducer(
       };
     }
     case "setFilters": {
+      if (action.filters.equals(state.filters)) {
+        return state;
+      }
       const allIssues = [...state.allIssuesMap.values()];
       return {
         ...state,
@@ -296,6 +319,9 @@ function reducer(
       };
     }
     case "setIssueOrder": {
+      if (action.issueOrder === state.issueOrder) {
+        return state;
+      }
       return {
         ...state,
         filteredIssues: sortBy(state.filteredIssues, order),
